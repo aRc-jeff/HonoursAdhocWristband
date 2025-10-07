b31fbdba6ad2a0ed4500a1d5f825cdde74bddc87
Timothy Szabo <a1852299@adelaide.edu.au>
2025-10-08 09:44:26 +1030

    added comments to rak4631 definitions

--- boards/rak4631/include/board.h
+++ boards/rak4631/include/board.h
@@ -10,14 +10,18 @@
  * @author      Timothy Szabo <a1852299@adelaide.edu.au>
  */
 
+// includes default CPU headers
 #include "cpu.h" 
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+// Define DIO2 pin
 #define DIO2_PIN                GPIO_PIN(1, 7)
 
+// Define LED and Antenna switch pins and masks 
+// mask is (1 << pin_number) regardless of port
 #define LED0_PIN                GPIO_PIN(1, 3)
 #define LED0_MASK               (1 << 3)
 #define LED1_PIN                GPIO_PIN(1, 4)
@@ -25,24 +29,28 @@ extern "C" {
 #define ANT_SW_PIN              GPIO_PIN(1, 5)
 #define ANT_SW_MASK             (1 << 5)
 
-#define GPIO_MASK_P1             (LED0_MASK | LED1_MASK | ANT_SW_MASK)
+// Collect all port 1 devices into a single mask (need separate one for port 0)
+#define GPIO_MASK_P1            (LED0_MASK | LED1_MASK | ANT_SW_MASK)
 
-#define LED0_ON                 (NRF_P1->OUTCLR = LED0_MASK)
-#define LED0_OFF                (NRF_P1->OUTSET = LED0_MASK)
+// Define macros to turn LED0 on/off
+#define LED0_ON                 (NRF_P1->OUTSET = LED0_MASK)
+#define LED0_OFF                (NRF_P1->OUTCLR = LED0_MASK)
 #define LED0_TOGGLE             (NRF_P1->OUT   ^= LED0_MASK)
 
-#define LED1_ON                 (NRF_P1->OUTCLR = LED1_MASK)
-#define LED1_OFF                (NRF_P1->OUTSET = LED1_MASK)
+// Define macros to turn LED1 on/off
+#define LED1_ON                 (NRF_P1->OUTSET = LED1_MASK)
+#define LED1_OFF                (NRF_P1->OUTCLR = LED1_MASK)
 #define LED1_TOGGLE             (NRF_P1->OUT   ^= LED1_MASK)
 
-#define ANT_SW_ON               (NRF_P1->OUTCLR = ANT_SW_MASK)
-#define ANT_SW_OFF              (NRF_P1->OUTSET = ANT_SW_MASK)
+// Define macros to turn Antenna switch on/off
+#define ANT_SW_ON               (NRF_P1->OUTSET = ANT_SW_MASK)
+#define ANT_SW_OFF              (NRF_P1->OUTCLR = ANT_SW_MASK)
 #define ANT_SW_TOGGLE           (NRF_P1->OUT   ^= ANT_SW_MASK)
 
 /**
  * @name        SX126X
  *
- * SX126X configuration.
+ * SX126X configuration, based on sx126x.h
  * @{
  */
 #define SX126X_PARAM_SPI                (SPI_DEV(0))
@@ -56,9 +64,10 @@ extern "C" {
 #define SX126X_PARAM_DIO3_MODE          SX126X_DIO3_TCXO
 #define SX126X_PARAM_TCXO_VOLTAGE       SX126X_TCXO_CTRL_3_0V
 #define SX126X_PARAM_TCXO_TIMEOUT       24
+/** @} */
 
-
-/* add LED pin defs if you want blinking: e.g. #define LED0_PIN <gpio> */
 #ifdef __cplusplus
 }
 #endif
+/** @} */
+
--- boards/rak4631/include/periph_conf.h
+++ boards/rak4631/include/periph_conf.h
@@ -1,34 +1,43 @@
 #pragma once
+
+// Include default cpu peripheral headers
 #include "periph_cpu.h"
+
+// Include cpu clock, counter and timer configurations 
+// (clock file depends on hardware config)
+// CLOCK_HFCLK  (32U): 32 mHz crystal main clock
+// CLOCK_LFCLK CLOCK_LFCLKSRC_SRC_Xtal: 32.768 kHz crystal
 #include "cfg_clock_32_1.h"
 #include "cfg_rtt_default.h"
 #include "cfg_timer_default.h"
 
 
-
-// /* HFCLK and LFCLK configuration for nRF52840 */
-// #define CLOCK_HFCLK        1
-// #define CLOCK_LFCLK        1
-
+//UART configuration is required for RIOT to build, we arent actually using it
 /**
  * @name    UART configuration
  * @{
  */
 static const uart_conf_t uart_config[] = {
     {
+        // controller selection
         .dev        = NRF_UARTE0,
+        // pin configuration
         .rx_pin = GPIO_PIN(0, 19),
         .tx_pin = GPIO_PIN(0, 20),
+        // if the modules are expecting these pins... they arent connected
 #ifdef MODULE_PERIPH_UART_HW_FC
         .rts_pin    = GPIO_UNDEF,
         .cts_pin    = GPIO_UNDEF,
 #endif
+        // interrupt config
         .irqn       = UARTE0_UART0_IRQn,
     },
 };
 
+// configure UART interrupt service routine
 #define UART_0_ISR          (isr_uart0)
 
+// set the number of configured uart devices
 #define UART_NUMOF          ARRAY_SIZE(uart_config)
 /** @} */
 
@@ -38,49 +47,51 @@ static const uart_conf_t uart_config[] = {
  */
 static const spi_conf_t spi_config[] = {
     {
+        // select SPI controller for SX1262 radio 
+        // (0, 1 and 2 are low speed, 3 is high speed)
         .dev  = NRF_SPIM1,
+
+        // configure spi pins
         .sclk = GPIO_PIN(1, 11),
         .mosi = GPIO_PIN(1, 12),
         .miso = GPIO_PIN(1, 13),
     },
     {
+        // select SPI controller for ws2812 LED
         .dev  = NRF_SPIM2,
         .mosi = GPIO_PIN(0, 17),
     }
 };
 
+// set the number of configured SPI devices
 #define SPI_NUMOF           ARRAY_SIZE(spi_config)
 /** @} */
 
+// I2C configured on the rak4631 but not actually used
+/**
+ * @name    I2C configuration
+ * @{
+ */
 static const i2c_conf_t i2c_config[] = {
     {
+        // select i2c controller
         .dev = NRF_TWIM0,
+        // set i2c pins
         .scl = GPIO_PIN(0, 14),
         .sda = GPIO_PIN(0, 13),
+        // set i2c speed
         .speed = I2C_SPEED_NORMAL
     },
     {
+        // select i2c controller
         .dev = NRF_TWIM1,
+        // set i2c pins
         .scl = GPIO_PIN(0, 25),
         .sda = GPIO_PIN(0, 24),
+        // set i2c speed
         .speed = I2C_SPEED_NORMAL
     },
 };
+// set the number of configured I2C devices
 #define I2C_NUMOF           ARRAY_SIZE(i2c_config)
-
-
-
-
-// /* Example: use SPI1 at 8 MHz for the SX1262 */
-// static const spi_conf_t spi_conf[SPI_NUMOF] = {
-//     /* SPI0 unused */
-//     { .dev = NRF_SPIM0, .sck_pin = GPIO_UNDEF, .mosi_pin = GPIO_UNDEF, .miso_pin = GPIO_UNDEF },
-//     /* SPI1 used by radio (RAK4631 wiring below) */
-//     { .dev = NRF_SPIM1, .sck_pin  = GPIO_PIN(1, 11),  /* SCK  */
-//                         .mosi_pin = GPIO_PIN(1, 12),  /* MOSI */
-//                         .miso_pin = GPIO_PIN(1, 13),  /* MISO */
-//       .freq = SPI_FREQ_8MHZ, .mode = SPI_MODE_0, .orc = 0xFF },
-//     /* SPI2 unused */
-//     { .dev = NRF_SPIM2, .sck_pin = GPIO_UNDEF, .mosi_pin = GPIO_UNDEF, .miso_pin = GPIO_UNDEF },
-// };
-// #define SPI_NUMOF ARRAY_SIZE(spi_config)
+/** @} */
